05d79b59f7b8d130c21805987d72b987c20f3d8e
Sarah Chow <sarahrch@usc.edu>
2021-02-21 14:24:31 -0500

    add assignment description

--- PA2/multithread_matrixmult.cpp
+++ PA2/multithread_matrixmult.cpp
@@ -4,6 +4,19 @@
 #include <unistd.h>
 using namespace std;
 
+/*
+Assignment: These threads share the process's resources but are able to execute independently.
+In this problem, you will implement a 10 threads C/C++ program to parallel compute matrix multiplication between a 1x5 matrix (matrix A) and 5x1 matrix (matrix B).
+You need to do the following steps (A and B are two matrixes of all fives)
+
+1. Create 10 threads and assign the matrixes elements into each thread (one for each).
+For example, assign the first element of A in thread 1, the second in thread 2, ...; assign the first element of B in thread 6, the second in thread 7...
+2. After each data element is assigned to each thread, use pipes to send the elements from one to another and calculate the multiplication results between these two.
+For example, send the element in thread 1 (the first element in A) to thread 6 and calculate the multiplication result. 
+3. Add these 5 multiplication results together. 
+4. Record execution time and calculation result in the report.
+*/
+
 // Must be run on Linux system
 
 void SendVals(int val, int &fd) {
