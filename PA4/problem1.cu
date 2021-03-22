83e1934e7b5b2ccafab52fecf409e23a1accdbde
Sarah Chow <sarahrch@usc.edu>
2021-03-22 14:32:33 -0700

    fixed memory issue with given code

--- PA4/problem1.cu
+++ PA4/problem1.cu
@@ -39,8 +39,9 @@ int main(int argc, char** argv){
 	// GIVEN CODE STARTS HERE ----
     FILE *fp;
 
-  	unsigned char *a = (unsigned char*) malloc (sizeof(unsigned char)*h*w);
-    
+  	unsigned char *a; // CHANGING THIS FROM GIVEN CODE TO AVOID MEMORY ACCESS ISSUES = (unsigned char*) malloc (sizeof(unsigned char)*h*w);
+	cudaMallocManaged(&a, sizeof(unsigned char)*h*w);
+
 	// the matrix is stored in a linear array in row major fashion
 	if (!(fp=fopen(input_file, "rb"))) {
 		printf("can not opern file\n");
@@ -70,8 +71,6 @@ int main(int argc, char** argv){
 		clusters[i] = 0;
 	}
 
-	std::cout << clusters[15] << std::endl;
-
 	// Run 30 iterations of k-means
 	for (int i = 0; i < 30; i++) {
 		// Need 800x800 = 640000 threads, CUDA GPUs run kernels using blocks of threads that are a multiple of 32 and no greater than 1024 (Compute Compatability 2.x and later) and 512 for 1.x
@@ -91,23 +90,18 @@ int main(int argc, char** argv){
 		int c4_vals = 0;
 
 		for (int j = 0; j < size; j++) {
-			std::cout << "testing all" << std::endl;
 			if (clusters[j] == 1) {
 				c1_total += (int)a[j];
 				c1_vals++;
-				std::cout << "1" <<std::endl;
 			} else if (clusters[j] == 2) {
 				c2_total += (int)a[j];
 				c2_vals++;
-				std::cout << "2" <<std::endl;
 			} else if (clusters[j] == 3) {
 				c3_total += (int)a[j];
 				c3_vals++;
-				std::cout << "3" <<std::endl;
 			} else if (clusters[j] == 4) {
 				c4_total += (int)a[j];
 				c4_vals++;
-				std::cout << "4" <<std::endl;
 			} else {
 				std::cout << clusters[j] << std::endl;
 			}
